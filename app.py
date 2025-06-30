34b6fe0badfd46174baac2953bc5e7477d2189bd
Eti-Hack <pro.dynamicbusiness@gmail.com>
2025-06-30 16:39:30 +0100

    app.py

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ mail = Mail(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587 
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'pro.dynamicbusiness@gmail.com'
-app.config['MAIL_PASSWORD'] = 'ibaktdasqirguyjk'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 
 mail = Mail(app)
 '''
